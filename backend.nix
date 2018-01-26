3dc87810ec282b51a7ced13e57ec535eee6eb2fc
Daniel Theriault <dannymt97@gmail.com>
2018-01-26 16:23:28 -0500

    New deployment specification. Permissions for the database are now set properly!

--- backend.nix
+++ backend.nix
@@ -5,45 +5,7 @@
 # This is a NixOps configuration file.
 # It specifies a server configuration, which NixOps can then deploy to arbitrary backends (physical hosts, VMs, AWS instances, etc.)
 
-let
-  myPython = pkgs.python3Full.buildEnv.override rec {
-    extraLibs = with pkgs.python3Packages; [
-      bcrypt
-      flask
-      flask_migrate
-      flask_sqlalchemy
-      psycopg2
-      sqlalchemy
-    ];
-  };
 
-  projectBackend = stdenv.mkDerivation rec {
-    name = "JD-backend-1.0";
-
-    src = pkgs.fetchgitLocal {
-      owner = "";
-      repo = "";
-      rev = "";
-      sha256 = "";
-    };
-
-    buildInputs = [
-      myPython
-    ];
-
-    installPhase = ''
-      python src/init.py
-    '';
-
-      # TODO: INIT PostGres
-      # - import models
-      # - db.create_all() + db.session.commit()
-
-    meta = {
-      platforms = stdenv.lib.platforms.linux;
-    };
-  };
-in
 {
   network.description = "Backend network for Georgia Tech CS Capstone Project";
 
@@ -51,12 +13,57 @@ in
   # So, until performance requires otherwise, the backend is a single host
   monolith = 
     { config, pkgs, ... }:
+    let
+      myPython = pkgs.python3Full.buildEnv.override rec {
+        extraLibs = with pkgs.python3Packages; [
+          bcrypt
+          flask
+          flask_migrate
+          flask_sqlalchemy
+          psycopg2
+          sqlalchemy
+        ];
+      };
+
+      backend = pkgs.stdenv.mkDerivation {
+        name = "7144Backend-0.1";
+
+        src = ./src;
+
+        buildInputs = [
+          myPython
+        ];
+
+        doCheck = false;
+
+        installPhase = ''
+          cp -r . $out/
+        '';
+
+        meta = {
+          platforms = pkgs.stdenv.lib.platforms.linux;
+        };
+      };
+    in
     {
-      services.postgresql.enable = true;
+      services.postgresql = {
+        enable = true;
+        initialScript = builtins.toFile "psql-init.sh" ''
+          CREATE DATABASE design_dev;
+          CREATE USER root;
+          CREATE USER gt7344;
+        '';
+        authentication = ''
+          local all all                trust
+          host  all all 127.0.0.1/32   trust
+          host  all all ::1/128        trust
+          host  all all 192.168.1.0/24 trust
+        '';
+      };
 
       environment.systemPackages = [
         myPython
-        projectBackend
+        backend 
       ];
 
      systemd.services.projectBackend = {
@@ -66,15 +73,15 @@ in
           DATABASE_URL = "postgresql+psycopg2:///design_dev"; 
         };
 
+        preStart = "python {backend}/init.py";
+
         serviceConfig = {
           Type = "forking";
-          ExecStart = "/usr/bin/env python";# TODO: backend binary path
+          ExecStart = "python {backend}/app.py";
           Restart = "on-failure";
         };
 
         wantedBy = [ "multi-user.target" ];
-        wants = [ "network-online.target" ];
-        after = [ "network-online.target" ];
       };
     };
 }
