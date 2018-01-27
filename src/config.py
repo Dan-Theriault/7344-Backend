27bd868021259670e221b479a2fcfade9155c5bd
Daniel Theriault <dannymt97@gmail.com>
2018-01-27 03:17:08 +0000

    Now use a hard string, instead of an env variable, for the database url.

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = True
     SECRET_KEY = "Not actually a secret ;_;"
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2:///design_dev"
     TOKEN_TIMEOUT = "???"  # set this to a timedelta
     # significant performance impact & not needed
     SQLALCHEMY_TRACK_MODIFICATIONS = False
